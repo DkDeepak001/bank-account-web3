0eeb866e37c527304f3d930fc1da237eb8dbc452
DkDeepak001 <Deepakeswar001@gmail.com>
2023-05-13 23:10:47 +0530

    test: :white_check_mark: should not allow user with more than 3 account

--- test/BankAccount.ts
+++ test/BankAccount.ts
@@ -68,4 +68,21 @@ describe("BankAccount", function (): void {
       bankAccount.connect(addr1).createAccount([addr1.address, addr1.address])
     ).to.be.revertedWith("you can't have duplicated owners");
   });
+
+  it("should not allow user with more than 3 account", async function (): Promise<void> {
+    const { bankAccount, addr1, addr2, addr3, addr4 } = await loadFixture(
+      deploy
+    );
+
+    await expect(
+      bankAccount
+        .connect(addr1)
+        .createAccount([
+          addr1.address,
+          addr2.address,
+          addr3.address,
+          addr4.address,
+        ])
+    ).to.be.reverted;
+  });
 });
