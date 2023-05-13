c47452a5cabebd41f7ef98b3479d318a47df655c
DkDeepak001 <Deepakeswar001@gmail.com>
2023-05-13 22:58:30 +0530

    test: :white_check_mark: added create single account

--- test/BankAccount.ts
+++ test/BankAccount.ts
@@ -2,6 +2,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
 
 type BankAccount<T> = {
   bankAccount: Contract;
@@ -24,6 +25,16 @@ describe("BankAccount", function (): void {
   describe("Deployment", function (): void {
     it("should deploy without error", async function (): Promise<void> {
       await loadFixture(deploy);
+    });
+  });
+
+  describe("Create Account", function (): void {
+    it("should create account without error", async function (): Promise<void> {
+      const { bankAccount, addr1 } = await loadFixture(deploy);
+      await bankAccount.connect(addr1).createAccount([addr1.address]);
+      const account = await bankAccount.connect(addr1).getUserAccounts();
+      console.log(account);
+      expect(account.length).to.equal(1);
     });
   });
 });
