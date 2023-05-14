bf4f97db7f68a09a8c44370840fcbfee162c5c43
DkDeepak001 <Deepakeswar001@gmail.com>
2023-05-14 12:03:10 +0530

    feat: :rocket: deployment script and writing ABI for contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,25 @@
-async function main() {}
+import { ethers } from "hardhat";
+import { promises as fs } from "fs";
+import { Contract } from "ethers";
 
+async function main() {
+  const BankAccount = await ethers.getContractFactory("BankAccount");
+  const bankAccount = await BankAccount.deploy();
+  await bankAccount.deployed();
+  await deploymentInfo(bankAccount);
+}
+
+async function deploymentInfo(contract: Contract) {
+  const data = {
+    contract: {
+      address: contract.address,
+      abi: contract.interface.format(),
+      signerAddress: contract.signer.address,
+    },
+  };
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile("deployment.json", content);
+}
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
